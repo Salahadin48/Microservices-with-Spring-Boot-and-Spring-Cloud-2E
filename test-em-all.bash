880efa9cc0877be8232fba9b1a69a1f29f990f37
Salahadin48 <salahadin48@outlook.com>
2021-11-26 21:55:29 -0600

    chapter04

--- test-em-all.bash
+++ test-em-all.bash
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 #
-# Simple usage:
+# Sample usage:
 #
-# HOST=localhost PORT=7000 ./test-em-all.bash
+#   HOST=localhost PORT=7000 ./test-em-all.bash
 #
 HOST='localhost'
-: ${PORT=7000}
+: ${PORT=8080}
 : ${PROD_ID_REVS_RECS=1}
 : ${PROD_ID_NOT_FOUND=13}
 : ${PROD_ID_NO_RECS=113}
 : ${PROD_ID_NO_REVS=213}
 
-function assertCurl(){
+function assertCurl() {
+
   local expectedHttpCode=$1
   local curlCmd="$2 -w \"%{http_code}\""
   local result=$(eval $curlCmd)
@@ -34,7 +35,8 @@ function assertCurl(){
   fi
 }
 
-function assertEqual(){
+function assertEqual() {
+
   local expected=$1
   local actual=$2
 
@@ -43,21 +45,65 @@ function assertEqual(){
     echo "Test OK (actual value: $actual)"
   else
     echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
+    exit 1
   fi
+}
+
+function testUrl() {
+  url=$@
+#  echo $($url -ks -f -0 /dev/null)
+  if $url -ks -f -0 /dev/null
+  then
+    return 0
+  else
+    return 1
+  fi;
+}
+
+function waitForService() {
+  url=$@
+  echo -n "Wait for: $url... "
+  n=0
+  until true $url
+  do
+    n=$((n + 1))
+    if [[ $n == 10 ]]
+    then
+      echo " Give up"
+      exit 1
+    else
+      sleep 3
+      echo -n ", retry #$n "
+    fi
+  done
+  echo "DONE, continues..."
 }
 
 set -e
 
+echo "Start Tests:" `date`
+
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 
-# verify that a normal request works, expect three recommendations and three reviews
+if [[ $@ == *"start"* ]]
+then
+  echo "Restarting the test environment..."
+  echo "$ docker-compose down --remove-orphans"
+  docker-compose down --remove-orphans
+  echo "$ docker-compose up -d"
+  docker-compose up -d
+fi
+
+waitForService curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS
+
+# Verify that a normal request works, expect three recommendations and three reviews
 assertCurl 200 "curl http://$HOST:$PORT/product-composite/$PROD_ID_REVS_RECS -s"
-# shellcheck disable=SC2086
 assertEqual $PROD_ID_REVS_RECS $(echo $RESPONSE | jq .productId)
 assertEqual 3 $(echo $RESPONSE | jq ".recommendations | length")
 assertEqual 3 $(echo $RESPONSE | jq ".reviews | length")
 
+# Verify that a 404 (Not Found) error is returned for a non-existing productId ($PROD_ID_NOT_FOUND)
 assertCurl 404 "curl http://$HOST:$PORT/product-composite/$PROD_ID_NOT_FOUND -s"
 assertEqual "No product found for productId: $PROD_ID_NOT_FOUND" "$(echo $RESPONSE | jq -r .message)"
 
@@ -80,5 +126,12 @@ assertEqual "\"Invalid productId: -1\"" "$(echo $RESPONSE | jq .message)"
 # Verify that a 400 (Bad Request) error error is returned for a productId that is not a number, i.e. invalid format
 assertCurl 400 "curl http://$HOST:$PORT/product-composite/invalidProductId -s"
 assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
+
+if [[ $@ == *"stop"* ]]
+then
+    echo "We are done, stopping the test environment..."
+    echo "$ docker-compose down"
+    docker-compose down
+fi
 
 echo "End, all tests OK:" `date`
